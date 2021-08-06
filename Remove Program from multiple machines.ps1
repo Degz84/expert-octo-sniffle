6ef09d7853aa375db5c65fb5a1a8783c935ca74c
Degz84 <88509664+Degz84@users.noreply.github.com>
2021-08-06 22:15:16 +0100

    Add files via upload
    Checking for 7-Zip on multiple machines and removing the program if it exists, machines are called via a text file placed in C:\Temp folder, This is all done via PS1 script.

---
+++ Remove Program from multiple machines.ps1
@@ -0,0 +1,8 @@
+﻿$Computers = Get-Content -path C:\Temp\computers.txt
+Foreach ($Computer in $Computers) {
+ Get-WmiObject -Class Win32_Product | Select-Object -Property Name
+ $MyApp = Get-WmiObject -Class Win32_Product | Where-Object{$_.Name -eq "7-Zip 19.00"}
+ $MyApp.Uninstall
+ Get-Package -Provider Programs -IncludeWindowsInstaller -Name "7-Zip 19.00"
+ Uninstall-Package -Name "7-Zip 19.00"
+ }
